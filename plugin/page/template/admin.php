42b1b5f9053fdfb68026359c61e72224ec34ed32
Christopher Boutillé <christopher.boutille@me.com>
2017-04-29 20:28:44 +0200

    Redesign de la page d'admin du plugin Page & Menu.

--- plugin/page/template/admin.php
+++ plugin/page/template/admin.php
@@ -5,9 +5,9 @@ include_once(ROOT.'admin/header.php');
 
 <?php if($mode == 'list'){ ?>
 <ul class="tabs_style">
-  <li><a class="button" href="index.php?p=page&amp;action=edit">Ajouter une page</a></li>
-  <li><a class="button" href="index.php?p=page&amp;action=edit&parent=1">Ajouter un item parent</a></li>
-  <li><a class="button" href="index.php?p=page&amp;action=edit&link=1">Ajouter un lien externe</a></li>
+  <li><a class="btn" href="index.php?p=page&amp;action=edit">Ajouter une page</a></li>
+  <li><a class="btn" href="index.php?p=page&amp;action=edit&parent=1">Ajouter un item parent</a></li>
+  <li><a class="btn" href="index.php?p=page&amp;action=edit&link=1">Ajouter un lien externe</a></li>
 </ul>
 <table>
   <thead>
@@ -65,124 +65,165 @@ include_once(ROOT.'admin/header.php');
 
 <?php if($mode == 'edit' && !$isLink && !$isParent && $pageItem->targetIs() != 'plugin'){ ?>
 <form method="post" action="index.php?p=page&amp;action=save">
-  <?php show::adminTokenField(); ?>
-  <input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
-  <input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
-  <p>
-      <input <?php if($pageItem->getIsHomepage()){ ?>checked<?php } ?> type="checkbox" name="isHomepage" /> Page d'accueil
-	</p>
-	<p>
-      <input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" /> Ne pas afficher dans le menu
-	</p>
-	<p>
-      <input <?php if($pageItem->getNoIndex()){ ?>checked<?php } ?> type="checkbox" name="noIndex" /> Interdire l'indexation
-  </p>
-  <p>
-      <label>Item parent</label><br>
-	  <select name="parent">
-	  <option value="">Aucun</option>
-	  <?php foreach($page->getItems() as $k=>$v) if($v->targetIs() == 'parent'){ ?>
-	  <option <?php if($v->getId() == $pageItem->getParent()){ ?>selected<?php } ?> value="<?php echo $v->getId(); ?>"><?php echo $v->getName(); ?></option>
-	  <?php } ?>
-	  </select>
-  </p>
-  <p>
-      <label>Nom</label><br>
-      <input type="text" name="name" value="<?php echo $pageItem->getName(); ?>" required="required" />
-  </p>
-  <p>
-      <label>Titre de page (optionel)</label><br>
-      <input type="text" name="mainTitle" value="<?php echo $pageItem->getMainTitle(); ?>" />
-  </p>
-  <p>
-      <label>Meta title (optionel)</label>
-      <input type="text" name="metaTitleTag" value="<?php echo $pageItem->getMetaTitleTag(); ?>" />
-  </p>
-  <p>
-      <label>Meta description (optionel)</label>
-      <input type="text" name="metaDescriptionTag" value="<?php echo $pageItem->getMetaDescriptionTag(); ?>" />
-  </p>
-  <p>
-      <label>Inclure un fichier .php au lieu du contenu (doit être présent dans le dossier de votre thème)
-	  <select name="file" class="large-3 columns">
-		  <option value="">--</option>
-		  <?php foreach($page->listTemplates() as $file){ ?>
-		  <option <?php if($file == $pageItem->getFile()){ ?>selected<?php } ?> value="<?php echo $file; ?>"><?php echo $file; ?></option>
-		  <?php } ?>
-	  </select>
-  </p>
-  <p>
-      <label>Contenu</label>
-			<textarea name="content" class="editor"><?php echo $pageItem->getContent(); ?></textarea>
-  </p>
-  <p>
-	<button type="submit" class="button success radius">Enregistrer</button>
-  </p>
+<?php show::adminTokenField(); ?>
+
+	<input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
+	<input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
+
+	<div class="field">
+		<label for="isHomepage">Page d'accueil</label>
+		<input <?php if($pageItem->getIsHomepage()){ ?>checked<?php } ?> type="checkbox" name="isHomepage" id="isHomepage" />
+	</div>
+
+	<div class="field">
+		<label for="isHidden">Ne pas afficher dans le menu</label>
+		<input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" id="isHidden" />
+	</div>
+
+	<div class="field">
+		<label for="noIndex">Interdire l'indexation</label>
+		<input <?php if($pageItem->getNoIndex()){ ?>checked<?php } ?> type="checkbox" name="noIndex" id="noIndex" />
+	</div>
+
+	<div class="field">
+		<label>Item parent</label>
+		<div>
+			<span class="drop-down"></span>
+			<select name="parent">
+				<option value="">Aucun</option>
+				<?php foreach($page->getItems() as $k=>$v) if($v->targetIs() == 'parent'){ ?>
+				<option <?php if($v->getId() == $pageItem->getParent()){ ?>selected<?php } ?> value="<?php echo $v->getId(); ?>"><?php echo $v->getName(); ?></option>
+				<?php } ?>
+			</select>
+		</div>
+	</div>
+
+	<div class="field">
+		<label for="name">Nom</label>
+		<input type="text" name="name" id="name" value="<?php echo $pageItem->getName(); ?>" required />
+	</div>
+
+	<div class="field">
+		<label for="mainTitle">Titre de page (optionel)</label>
+		<input type="text" name="mainTitle" id="mainTitle" value="<?php echo $pageItem->getMainTitle(); ?>" />
+	</div>
+
+	<div class="field">
+		<label for="metaTitleTag">Meta title (optionel)</label>
+		<input type="text" name="metaTitleTag" id="metaTitleTag" value="<?php echo $pageItem->getMetaTitleTag(); ?>" />
+	</div>
+
+	<div class="field">
+		<label for="metaDescriptionTag">Meta description (optionel)</label>
+		<input type="text" name="metaDescriptionTag" id="metaDescriptionTag" value="<?php echo $pageItem->getMetaDescriptionTag(); ?>" />
+	</div>
+
+	<div class="field">
+		<label>Inclure un fichier .php au lieu du contenu (doit être présent dans le dossier de votre thème)</label>
+		<div>
+			<span class="drop-down"></span>
+			<select name="file">
+				<option value="">--</option>
+				<?php foreach($page->listTemplates() as $file){ ?>
+				<option <?php if($file == $pageItem->getFile()){ ?>selected<?php } ?> value="<?php echo $file; ?>">
+					<?php echo $file; ?>
+				</option>
+				<?php } ?>
+			</select>
+		</div>
+	</div>
+
+	<div class="field">
+		<label for="content">Contenu</label>
+		<textarea name="content" id="content" class="editor"><?php echo $pageItem->getContent(); ?></textarea>
+	</div>
+
+	<button class="btn" type="submit" name="submit">Enregistrer</button>
+
 </form>
 <?php } ?>
 
 <?php if($mode == 'edit' && ($isLink || $pageItem->targetIs() == 'plugin')){ ?>
 <form method="post" action="index.php?p=page&amp;action=save">
-  <?php show::adminTokenField(); ?>
-  <input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
-  <input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
-  <p>
-      <input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" /> <label for="isHidden">Ne pas afficher dans le menu</label>
-  </p>
-  <p>
-      <label>Item parent</label><br>
-	  <select name="parent">
-	  <option value="">Aucun</option>
-	  <?php foreach($page->getItems() as $k=>$v) if($v->targetIs() == 'parent'){ ?>
-	  <option <?php if($v->getId() == $pageItem->getParent()){ ?>selected<?php } ?> value="<?php echo $v->getId(); ?>"><?php echo $v->getName(); ?></option>
-	  <?php } ?>
-	  </select>
-  </p>
-  <p>
-      <label>Nom</label><br>
-      <input type="text" name="name" value="<?php echo $pageItem->getName(); ?>" required="required" />
-  </p>
-  <?php if($pageItem->targetIs() == 'plugin'){ ?>
-  <p>
-      <label>Cible : <?php echo $pageItem->getTarget(); ?></label>
-      <input style="display:none;" type="text" name="target" value="<?php echo $pageItem->getTarget(); ?>" />
-  </p>
-  <?php } else{ ?>
-  <p>
-      <label>Cible</label><br>
-      <input placeholder="Example : http://www.google.com" <?php if($pageItem->targetIs() == 'plugin'){ ?>readonly<?php } ?> type="url" name="target" value="<?php echo $pageItem->getTarget(); ?>" required="required" />
-  </p>
-  <?php } ?>
-  <p>
-      <label>Ouverture</label><br>
-	  <select name="targetAttr">
-		<option value="_self" <?php if($pageItem->getTargetAttr() == '_self'){ ?>selected<?php } ?>>Même fenêtre</option>
-		<option value="_blank" <?php if($pageItem->getTargetAttr() == '_blank'){ ?>selected<?php } ?>>Nouvelle fenêtre</option>
-	  </select>
-  </p>
-  <p>
-	<button type="submit" class="button success radius">Enregistrer</button>
-  </p>
+<?php show::adminTokenField(); ?>
+
+	<input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
+	<input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
+
+	<div class="field">
+		<label for="isHidden">Ne pas afficher dans le menu</label>
+		<input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" id="isHidden" />
+	</div>
+
+	<div class="field">
+		<label>Item parent</label>
+		<div>
+			<span class="drop-down"></span>
+			<select name="parent">
+				<option value="">Aucun</option>
+				<?php foreach($page->getItems() as $k=>$v) if($v->targetIs() == 'parent'){ ?>
+				<option <?php if($v->getId() == $pageItem->getParent()){ ?>selected<?php } ?> value="<?php echo $v->getId(); ?>"><?php echo $v->getName(); ?></option>
+				<?php } ?>
+			</select>
+		</div>
+	</div>
+
+	<div class="field">
+		<label for="name">Nom</label>
+		<input type="text" name="name" id="name" value="<?php echo $pageItem->getName(); ?>" required />
+	</div>
+
+	<?php if($pageItem->targetIs() == 'plugin'){ ?>
+
+	<div class="field">
+		<label>Cible : <?php echo $pageItem->getTarget(); ?></label>
+		<input style="display:none;" type="text" name="target" value="<?php echo $pageItem->getTarget(); ?>" />
+	</div>
+
+	<?php } else{ ?>
+
+	<div class="field">
+		<label for="target">Cible</label>
+		<input placeholder="Example : http://www.google.com" <?php if($pageItem->targetIs() == 'plugin'){ ?>readonly<?php } ?> type="url" name="target" id="target" value="<?php echo $pageItem->getTarget(); ?>" required />
+	</div>
+
+	<?php } ?>
+
+	<div class="field">
+		<label>Ouverture</label>
+		<div>
+			<span class="drop-down"></span>
+			<select name="targetAttr">
+				<option value="_self" <?php if($pageItem->getTargetAttr() == '_self'){ ?>selected<?php } ?>>Même fenêtre</option>
+				<option value="_blank" <?php if($pageItem->getTargetAttr() == '_blank'){ ?>selected<?php } ?>>Nouvelle fenêtre</option>
+			</select>
+		</div>
+	</div>
+
+	<button class="btn" type="submit" name="submit">Enregistrer</button>
+
 </form>
 <?php } ?>
 
 <?php if($mode == 'edit' && $isParent){ ?>
 <form method="post" action="index.php?p=page&amp;action=save">
-  <?php show::adminTokenField(); ?>
-  <input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
-  <input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
-  <input type="hidden" name="target" value="javascript:" />
-  
-  <p>
-      <label>Nom</label><br>
-      <input type="text" name="name" value="<?php echo $pageItem->getName(); ?>" required="required" />
-  </p>
-  <p>
-      <input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" /> <label for="isHidden">Ne pas afficher dans le menu</label>
-  </p>
-  <p>
-	<button type="submit" class="button success radius">Enregistrer</button>
-  </p>
+<?php show::adminTokenField(); ?>
+	<input type="hidden" name="id" value="<?php echo $pageItem->getId(); ?>" />
+	<input type="hidden" name="position" value="<?php echo $pageItem->getPosition(); ?>" />
+	<input type="hidden" name="target" value="javascript:" />
+
+	<div class="field">
+		<label for="name">Nom</label>
+		<input type="text" name="name" id="name" value="<?php echo $pageItem->getName(); ?>" required />
+	</div>
+
+	<div class="field">
+		<label for="isHidden">Ne pas afficher dans le menu</label>
+		<input <?php if($pageItem->getIsHidden()){ ?>checked<?php } ?> type="checkbox" name="isHidden" id="isHidden" />
+	</div>
+
+	<button class="btn" type="submit" name="submit">Enregistrer</button>
+
 </form>
 <?php } ?>
 
