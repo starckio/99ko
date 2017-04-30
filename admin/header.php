f226f944d5e03b16afede0e8cc3361110cc9be42
Christopher Boutillé <christopher.boutille@me.com>
2017-04-30 22:28:22 +0200

    Ajustement 🛠

--- admin/header.php
+++ admin/header.php
@@ -36,7 +36,8 @@
 <aside role="seealso" class="sidebar">
 	<ul class="menu cf">
 		<?php foreach($pluginsManager->getPlugins() as $k=>$v) if($v->getConfigVal('activate') && $v->getAdminFile()){ ?>
-		<li class="<?php if($v->isRequired()){ ?>last<?php } ?>">
+		<li<?php if($v->isRequired()){ ?> class="last"<?php } ?>>
+		
 			<a href="index.php?p=<?php echo $v->getName(); ?>"><?php echo $v->getInfoVal('name'); ?></a>
 		</li>
 		<?php } ?>
