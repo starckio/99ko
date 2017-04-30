9f02e399c53184f2f68602de7fe79035bdd74a63
Christopher Boutillé <christopher.boutille@me.com>
2017-04-30 04:36:58 +0200

    Ajustement de la partie "paramètre".

--- plugin/contact/template/param.php
+++ plugin/contact/template/param.php
@@ -4,14 +4,15 @@
 
 <?php show::adminTokenField(); ?>
 
-	<div class="field">
-		<label for="copy">Destinataire en copie</label>
-		<input type="text" name="copy" value="<?php echo $runPlugin->getConfigVal('copy'); ?>" />
-	</div>
-
-	<div class="field">
-		<label for="label">Titre de page</label>
-		<input type="text" name="label" value="<?php echo $runPlugin->getConfigVal('label'); ?>" required />
+	<div class="cf">
+		<div class="field one">
+			<label for="copy">Destinataire en copie</label>
+			<input type="text" name="copy" value="<?php echo $runPlugin->getConfigVal('copy'); ?>" />
+		</div>
+		<div class="field two">
+			<label for="label">Titre de page</label>
+			<input type="text" name="label" value="<?php echo $runPlugin->getConfigVal('label'); ?>" required />
+		</div>
 	</div>
 
 	<button class="btn" type="submit" name="submit">Enregistrer</button>
