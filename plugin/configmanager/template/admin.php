988b0d91cbfc0568b23a864d63e692d4a5c036dd
Christopher Boutillé <christopher.boutille@me.com>
2017-04-30 16:55:40 +0200

    Structure paramètre (essai) 🛠

--- plugin/configmanager/template/admin.php
+++ plugin/configmanager/template/admin.php
@@ -68,46 +68,6 @@ include_once(ROOT.'admin/header.php');
 	<div class="field">
 		<label for="_adminPwd2">Confirmation</label>
 		<input type="password" name="_adminPwd2" value="" autocomplete="off" placeholder="••••••••" />
-	</div>
-    <hr />
-
-	<h3>Configuration avançés</h3>
-
-	<div class="field">
-		<input <?php if($core->getConfigVal('debug')){ ?>checked<?php } ?> type="checkbox" name="debug" id="debug" />
-		<label for="debug">Mode débogage</label>
-	</div>
-
-	<div class="field">
-		<input type="checkbox" onclick="updateHtaccess('<?php echo $rewriteBase; ?>');" <?php if($core->getConfigVal('urlRewriting')){ ?>checked<?php } ?> name="urlRewriting" id="urlRewriting" />
-		<label for="urlRewriting">Réécriture d'URL</label>
-	</div>
-
-	<div class="field">
-		<input type="checkbox" onclick="updateHtaccess('<?php echo $rewriteBase; ?>');" <?php if($core->getConfigVal('htaccessOptimization')){ ?>checked<?php } ?> name="htaccessOptimization" id="htaccessOptimization" />
-		<label for="htaccessOptimization">Optimisations .htaccess</label>
-	</div>
-
-	<div class="field">
-		<label for="siteUrl">URL du site (sans slash final)</label>
-		<input type="text" name="siteUrl" id="siteUrl" value="<?php echo $core->getConfigVal('siteUrl'); ?>" />
-	</div>
-
-	<div class="field">
-		<label for="urlSeparator">Séparateur d'URL</label>
-		<div>
-			<span class="drop-down"></span>
-			<select name="urlSeparator">
-				<option <?php if($core->getConfigVal('urlSeparator') == ','){ ?>selected<?php } ?> value=",">virgule</option>
-				<option <?php if($core->getConfigVal('urlSeparator') == '.'){ ?>selected<?php } ?> value=".">point</option>
-				<option <?php if($core->getConfigVal('urlSeparator') == '/'){ ?>selected<?php } ?> value="/">slash</option>
-			</select>
-		</div>
-	</div>
-
-	<div class="field">
-		<label for="htaccess">.htaccess</label>
-		<textarea name="htaccess" id="htaccess"><?php echo $core->getHtaccess(); ?></textarea>
 	</div>
 
 	<button class="btn" type="submit" name="submit">Enregistrer</button>
