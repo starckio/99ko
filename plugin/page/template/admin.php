a7c9c4fd0844956c52845606b07b7f695eb9ec39
Christopher Boutillé <christopher.boutille@me.com>
2017-04-30 17:25:02 +0200

    Ajustement CSS 🛠
    Donne une couleur différente au SVG au survol du lien.

--- plugin/page/template/admin.php
+++ plugin/page/template/admin.php
@@ -38,7 +38,8 @@ include_once(ROOT.'admin/header.php');
 			<a href="index.php?p=page&action=down&id=<?php echo $pageItem->getId(); ?>&token=<?php echo administrator::getToken(); ?>"><img src="icons/chevron-bottom.svg" alt="donw" /></a>
 			<a href="index.php?p=page&amp;action=edit&amp;id=<?php echo $pageItem->getId(); ?>"><img src="icons/edit.svg" alt="edit" /></a>
 			<?php if(!$pageItem->getIsHomepage() && $pageItem->targetIs() != 'plugin'){ ?>
-			<a class="red" href="index.php?p=page&amp;action=del&amp;id=<?php echo $pageItem->getId(). '&amp;token=' .administrator::getToken(); ?>" onclick = "if(!confirm('Supprimer cet élément ?')) return false;"><img src="icons/trash.svg" alt="trash" /></a>
+			<a class="red" href="index.php?p=page&amp;action=del&amp;id=<?php echo $pageItem->getId(). '&amp;token=' .administrator::getToken(); ?>" onclick = "if(!confirm('Supprimer cet élément ?')) return false;"><svg id="i-trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6" />
+			</svg></a>
 			<?php } else{ ?>
 			<div class="locked"><img src="icons/lock.svg" alt="lock" /></div>
 			<?php } ?>
@@ -65,7 +66,8 @@ include_once(ROOT.'admin/header.php');
 			<a href="index.php?p=page&action=up&id=<?php echo $pageItemChild->getId(); ?>&token=<?php echo administrator::getToken(); ?>"><img src="icons/chevron-top.svg" alt="up" /></a>
 			<a href="index.php?p=page&action=down&id=<?php echo $pageItemChild->getId(); ?>&token=<?php echo administrator::getToken(); ?>"><img src="icons/chevron-bottom.svg" alt="down" /></a>
 			<a href="index.php?p=page&amp;action=edit&amp;id=<?php echo $pageItemChild->getId(); ?>"><img src="icons/edit.svg" alt="edit" /></a>
-			<?php if(!$pageItemChild->getIsHomepage() && $pageItemChild->targetIs() != 'plugin'){ ?><a class="red" href="index.php?p=page&amp;action=del&amp;id=<?php echo $pageItemChild->getId(). '&amp;token=' .administrator::getToken(); ?>" onclick = "if(!confirm('Supprimer cet élément ?')) return false;"><img src="icons/trash.svg" alt="trash" /></a><?php } ?>
+			<?php if(!$pageItemChild->getIsHomepage() && $pageItemChild->targetIs() != 'plugin'){ ?><a class="red" href="index.php?p=page&amp;action=del&amp;id=<?php echo $pageItemChild->getId(). '&amp;token=' .administrator::getToken(); ?>" onclick = "if(!confirm('Supprimer cet élément ?')) return false;"><svg id="i-trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" height="24" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M28 6 L6 6 8 30 24 30 26 6 4 6 M16 12 L16 24 M21 12 L20 24 M11 12 L12 24 M12 6 L13 2 19 2 20 6" />
+			</svg></a><?php } ?>
 		</nav>
 	</div>
 
