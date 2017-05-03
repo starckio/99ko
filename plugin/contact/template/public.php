3d09f129fbd496f41fd40da096035d21b504ffdb
Christopher Boutillé <christopher.boutille@me.com>
2017-05-03 02:23:55 +0200

    Redesign du template contact (public).

--- plugin/contact/template/public.php
+++ plugin/contact/template/public.php
@@ -1,33 +1,40 @@
-<?php
-defined('ROOT') OR exit('No direct script access allowed');
-include_once(ROOT.'theme/'.$core->getConfigVal('theme').'/header.php');
-echo $runPlugin->getConfigVal('content1');
-?>
-
-<?php show::msg($msg, $msgType); ?>
-<form method="post" action="<?php echo $core->makeUrl('contact', array('action' => 'send')); ?>">
-  <p>
-    <label>Nom</label><br>
-    <input required="required" type="text" name="name" value="<?php echo $name; ?>" />
-  </p>	
-  <p>
-    <label>Prénom</label><br>
-    <input required="required" type="text" name="firstname" value="<?php echo $firstname; ?>" />
-  </p>
-  <p>
-    <label>Email</label><br>
-    <input required="email" type="email" name="email" value="<?php echo $email; ?>" />
-  </p>
-  <p>
-    <label>Message</label><br>
-    <textarea required="required" name="message"><?php echo $message; ?></textarea>
-  </p>
-  <p>
-    <input type="submit" value="Envoyer" />
-  </p>
-</form>
-
-<?php
-echo $runPlugin->getConfigVal('content2');
-include_once(ROOT.'theme/'.$core->getConfigVal('theme').'/footer.php');
+<?php
+defined('ROOT') OR exit('No direct script access allowed');
+include_once(ROOT.'theme/'.$core->getConfigVal('theme').'/header.php');
 ?>
+
+<div class="top-contact">
+	<?php echo $runPlugin->getConfigVal('content1'); ?>
+</div>
+
+<div id="form" class="cf">
+	<?php show::msg($msg, $msgType); ?>
+	<form method="post" action="<?php echo $core->makeUrl('contact', array('action' => 'send')); ?>">		<div class="field">
+			<label for="name">Nom</label>
+			<input type="text" name="name" id="name" value="<?php echo $name; ?>" />
+		</div>
+
+		<div class="field">
+			<label for="firstname">Prénom</label>
+			<input type="text" name="firstname" id="firstname" value="<?php echo $firstname; ?>" />
+		</div>
+
+		<div class="field">
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" value="<?php echo $email; ?>" />
+		</div>
+
+		<div class="field">
+			<label for="message">Message</label>
+			<textarea type="text" name="message" id="message" /><?php echo $message; ?></textarea>
+		</div>
+
+		<button class="btn" type="submit" name="submit">Envoyer</button>
+	</form>
+</div>
+
+<div class="bottom-contact">
+	<?php echo $runPlugin->getConfigVal('content2'); ?>
+</div>
+
+<?php include_once(ROOT.'theme/'.$core->getConfigVal('theme').'/footer.php'); ?>
