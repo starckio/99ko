b2843ba1550f63b8ff9e536fa555788cda91fcf1
Christopher Boutillé <christopher.boutille@me.com>
2017-05-03 02:21:42 +0200

    Reduction du script.

--- admin/scripts.js
+++ admin/scripts.js
@@ -1,18 +1,10 @@
 $(document).ready(function () {
+
 	$(".msg").delay(5000).fadeOut('slow');
-	// tri menu
-	var elem = $('#navigation').find('li').sort(sortMe);
-	function sortMe(a, b){
-			return a.className > b.className;
-	}
-	$('#navigation').append(elem);
-	// param
+
 	$('#param_link').click(function(){
-		if($('#param_panel').css('display') == 'none'){
-			$('#param_panel').slideDown();
-		}
-		else{
-			$('#param_panel').slideUp();
-		}
+	    $(this).toggleClass('active');
+	    $('#param_panel').toggleClass('active');
 	});
+
 });
